6ea26605b715e5a7e5d36c06d123348e43a6afc7
Juliette Le Goff <jle-goff@c1r11s6.42lisboa.com>
2023-12-06 17:14:46 +0000

    fixed infinite loop w/ invalid command

--- bonus/pipex_bonus.c
+++ bonus/pipex_bonus.c
@@ -6,26 +6,16 @@
 /*   By: jle-goff <jle-goff@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/28 17:17:17 by jle-goff          #+#    #+#             */
-/*   Updated: 2023/11/28 17:29:02 by jle-goff         ###   ########.fr       */
+/*   Updated: 2023/12/06 16:51:52 by jle-goff         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "pipex_bonus.h"
 
-// void	read_from_pipe(int fd, char *filename, char *cmd_path, char **argument)
-// {
-// 	int		file2;
-
-// 	dup2(fd, 0);
-// 	close(fd);
-// 	execve(cmd_path, argument, NULL);
-// 	printf("Parent execve error\n");
-// }
-
 void	error_quit(int type)
 {
 	if (type == 1)
-		write(1, "Usage: ./pipex file1 cmd1 cmd2 file2\n", 37);
+		write(1, "Usage: ./pipex file1 cmd1 cmd2 ... file2\n", 41);
 	if (type == 2)
 		perror("pipe");
 	if (type == 3)
@@ -40,6 +30,7 @@ void	write_to_pipe(int fd, char *cmd_path, char **argument)
 {
 
 	dup2(fd, 1);
+	close(fd);
 	execve(cmd_path, argument, NULL);
 	free(cmd_path);
 }
@@ -85,6 +76,16 @@ char	*get_command_path(char *command, char **env)
 				}
 				i++;
 			}
+			i = -1;
+			while (dirs[++i])
+				free(dirs[i]);
+			free(dirs);
+			i = -1;
+			while (new_cmd[++i])
+				free(new_cmd[i]);
+			free(new_cmd);
+			free(cmd_path);
+			return (NULL);
 		}
 		i++;
 	}
@@ -119,7 +120,9 @@ int	main(int argc, char **argv, char **env)
 			cmd_path = get_command_path(argv[i], env);
 			if (!cmd_path)
 				error_quit(4);
-			write_to_pipe(fd[1], cmd_path, create_arg(argv[i]), argv[1]);
+			close(fileout);
+			close(filein);
+			write_to_pipe(fd[1], cmd_path, create_arg(argv[i]));
 		}
 		else
 		{
@@ -130,6 +133,8 @@ int	main(int argc, char **argv, char **env)
 		i++;
 	}
 	dup2(fileout, 1);
+	close(fileout);
+	close(filein);
 	cmd_path = get_command_path(argv[i], env);
 	execve(cmd_path, create_arg(argv[i]), env);
 	exit(0);
